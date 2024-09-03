067b29e0fb8e93db99025099a65cbe067be747d9
Nikolina Baros <nikolina.baros12@gmail.com>
2024-09-03 14:14:59 +0200

    Dokumentovana klasa Operacija

--- src/main/java/rs/ac/bg/fon/ai/kozmeticki_salon_zajednicki/config/Operacija.java
+++ src/main/java/rs/ac/bg/fon/ai/kozmeticki_salon_zajednicki/config/Operacija.java
@@ -1,36 +1,121 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
+
+
+
+
 package rs.ac.bg.fon.ai.kozmeticki_salon_zajednicki.config;
 
 import java.io.Serializable;
 
 /**
- *
- * @author ninic
+ * Enumeracija koja predstavlja razlicite operacije koje klijent moze izvršiti u okviru
+ * softvera za upravljanje radom kozmetickog salona.
+ * 
+ * Svaka konstanta u ovoj enumeraciji predstavlja specifičnu operaciju koja
+ * se koristi unutar sistema.
+ * 
+ * @author Nikolina Baros
  */
 public enum Operacija implements Serializable{
 
+    /**
+     * Operacija prijave korisnika.
+     */
+    
     LOGIN,
+    /**
+     * Operacija pretrage klijenata na osnovu zadatih kriterijuma.
+     */
+ 
     NADJI_KLIJENTE, 
+    /**
+     * Operacija učitavanja podataka o određenom klijentu.
+     */
     UCITAJ_KLIJENTA,
-    IZBRISI_KLIJENTA, 
+    
+    /**
+     * Operacija brisanja klijenta iz sistema.
+     */
+    IZBRISI_KLIJENTA,
+    
+     /**
+     * Operacija čuvanja (ažuriranja ili dodavanja) podataka o klijentu.
+     */
     ZAPAMTI_KLIJENTA,
+    
+    /**
+     * Operacija pretrage usluga na osnovu zadatih kriterijuma.
+     */
     NADJI_USLUGE,
+    
+     /**
+     * Operacija učitavanja podataka o određenoj usluzi.
+     */
     UCITAJ_USLUGU,
+    
+    /**
+     * Operacija učitavanja podataka o određenoj rezervaciji.
+     */
     UCITAJ_REZERVACIJU,
+    
+    /**
+     * Operacija brisanja usluge iz sistema.
+     */
     IZBRISI_USLUGU, 
+    
+     /**
+     * Operacija učitavanja liste svih tipova usluga.
+     */
     UCITAJ_LISTU_TIPOVA_USLUGE, 
+    
+    /**
+     * Operacija čuvanja (ažuriranja ili dodavanja) podataka o usluzi.
+     */
     ZAPAMTI_USLUGU, 
+    
+     /**
+     * Operacija pretrage rezervacija na osnovu zadatih kriterijuma.
+     */
     NADJI_REZERVACIJE, 
+    
+     /**
+     * Operacija učitavanja liste svih popusta.
+     */
     UCITAJ_LISTU_POPUSTA, 
-    ZAPAMTI_REZERVACIJU, 
+    
+     /**
+     * Operacija čuvanja (ažuriranja ili dodavanja) podataka o rezervaciji.
+     */
+    ZAPAMTI_REZERVACIJU,
+    
+    /**
+     * Operacija brisanja rezervacije iz sistema.
+     */
     IZBRISI_REZERVACIJU, 
+    
+      /**
+     * Operacija učitavanja liste svih usluga.
+     */
     UCITAJ_LISTU_USLUGA,
+    
+    /**
+     * Operacija učitavanja liste svih klijenata.
+     */
     UCITAJ_LISTU_KLIJENATA, 
+    
+    /**
+     * Operacija odjave korisnika.
+     */
     LOGOUT,
-    UCITAJ_SVE_STATISTIKE, ZAPAMTI_STATISTIKE
+    
+    /**
+     * Operacija učitavanja svih statističkih podataka.
+     */
+    UCITAJ_SVE_STATISTIKE, 
+    
+    /**
+     * Operacija čuvanja (ažuriranja ili dodavanja) statističkih podataka.
+     */
+    ZAPAMTI_STATISTIKE
     
    
     
