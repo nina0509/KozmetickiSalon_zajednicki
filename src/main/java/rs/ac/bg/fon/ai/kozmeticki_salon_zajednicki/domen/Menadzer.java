9a1fc3b2b7cfe0574c84773b9fd51c25c730e75e
Nikolina Baros <nikolina.baros12@gmail.com>
2024-09-03 15:58:35 +0200

    Dokumentovana klasa Menadzer

--- src/main/java/rs/ac/bg/fon/ai/kozmeticki_salon_zajednicki/domen/Menadzer.java
+++ src/main/java/rs/ac/bg/fon/ai/kozmeticki_salon_zajednicki/domen/Menadzer.java
@@ -9,21 +9,55 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-/**
- *
- * @author ninic
- */
-public class Menadzer implements OpstiDomenskiObjekat {
 
+/**
+ * Predstavlja menadzer u sistemu kozmetičkog salona sa id-jem, 
+ * korisnickim imenom, lozinkom, imenom i prezimenom.
+ * 
+ * Implementira interfejs OpstiDomenskiObjekat, koji definiše metode za 
+ * operacije nad bazom podataka.
+ * 
+ * @author Nikolina Baros
+ */
+
+public class Menadzer implements OpstiDomenskiObjekat {
+/**
+     * Jedinstveni identifikator menadžera.
+     */
     private int id;
+    
+     /**
+     * Korisničko ime menadžera.
+     */
     private String username;
+    /**
+     * Lozinka menadžera.
+     */
     private String password;
+     /**
+     * Ime menadžera.
+     */
     private String ime;
+     /**
+     * Prezime menadžera.
+     */
     private String prezime;
 
+    /**
+     *Podrazumevani, prazan konstruktor za kreiranje nove instance klase Menadzer.
+     */
     public Menadzer() {
     }
 
+     /**
+     * Konstruktor sa parametrima za kreiranje novog Menadzera.
+     * 
+     * @param id Jedinstveni identifikator menadžera.
+     * @param username Korisničko ime menadžera.
+     * @param password Lozinka menadžera.
+     * @param ime Ime menadžera.
+     * @param prezime Prezime menadžera.
+     */
     public Menadzer(int id, String username, String password, String ime, String prezime) {
         this.id = id;
         this.username = username;
@@ -32,26 +66,57 @@ public class Menadzer implements OpstiDomenskiObjekat {
         this.prezime = prezime;
     }
 
+    /**
+     * Vraća jedinstveni identifikator menadžera.
+     * 
+     * @return Jedinstveni identifikator menadžera kao ceo broj.
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * Postavlja ID menadzera na prosledjenu vrednost.
+     * 
+     * @param id Novi ID menadzera kao ceo broj.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Vraća ime menadžera.
+     * 
+     * @return Ime menadžera kao String.
+     */
     public String getIme() {
         return ime;
     }
 
+     /**
+     * Postavlja ime menadzera na prosledjenu vrednost.
+     * 
+     * @param ime Novo ime menadzera kao String.
+     */
     public void setIme(String ime) {
         this.ime = ime;
     }
 
+    
+     /**
+     * Vraća prezime menadžera.
+     * 
+     * @return Prezime menadžera kao String.
+     */
     public String getPrezime() {
         return prezime;
     }
 
+    
+    /**
+     * Postavlja prezime menadzera na prosledjenu vrednost.
+     * 
+     * @param prezime Novo prezime menadzera kao String.
+     */
     public void setPrezime(String prezime) {
         this.prezime = prezime;
     }
@@ -80,27 +145,61 @@ public class Menadzer implements OpstiDomenskiObjekat {
         return Objects.equals(this.password, other.password);
     }
 
+    /**
+     * Vraća korisničko ime menadžera.
+     * 
+     * @return Korisničko ime menadžera kao String.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Postavlja korisničko ime menadzera na prosledjenu vrednost.
+     * 
+     * @param username Novo korisničko ime menadzera kao String.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    
+    /**
+     * Vraća lozinku menadžera.
+     * 
+     * @return Lozinka menadžera kao String.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Postavlja lozinku menadzera na prosledjenu vrednost.
+     * 
+     * @param password Nova lozinka menadzera kao String.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    
+  /**
+     * Vraca naziv tabele u bazi podataka koja odgovara klasi Menadzer.
+     * 
+     * @return Naziv tabele "menadzer" kao String.
+     */
     @Override
     public String vratiNazivTabele() {
         return "menadzer";
     }
 
+     /**
+     * Kreira listu menadzera na osnovu rezultata upita iz baze podataka.
+     * 
+     * @param rs Rezultat upita iz baze podataka u obliku ResultSet-a.
+     * @return Lista menadžera kreirana na osnovu rezultata upita.
+     * @throws Exception u slucaju greske tokom obrade rezultata upita.
+     */
     @Override
     public List<OpstiDomenskiObjekat> vratiListu(ResultSet rs) throws Exception {
         List<OpstiDomenskiObjekat> lista = new ArrayList<>();
@@ -119,30 +218,59 @@ public class Menadzer implements OpstiDomenskiObjekat {
         return lista;
     }
 
+    /**
+     * Vraca nazive kolona koje se koriste prilikom unosa podataka u tabelu Menadžer u bazi.
+     * 
+     * @return Nazivi kolona tabele menadzer kao String.
+     */
     @Override
     public String vratiKoloneZaInsert() {
         return null;
     }
 
+    
+    
+    /**
+     * Vraca vrednosti koje se koriste prilikom azuriranja podataka u tabeli Menadžer u bazi.
+     * 
+     * @return Vrednosti za azuriranje podataka u tabeli Menadžer kao String.
+     */
     @Override
     public String vratiVrednostiZaUpdate() {
         return null;
 
     }
 
+    
+    /**
+     * Vraca vrednosti koje se koriste prilikom unosa podataka u tabelu Menadžer u bazi.
+     * 
+     * @return Vrednosti za unos podataka u tabelu Menadžer u bazi kao String.
+     */
     @Override
     public String vratiVrednostiZaInsert() {
         return null;
 
     }
 
+    
+    /**
+     * Vraca primarni kljuc koji se koristi za identifikaciju zapisa u tabeli Menadžer u bazi .
+     * 
+     * @return Primarni kljuc tabele menadžer kao String.
+     */
     @Override
     public String vratiPrimarniKljuc() {
         return "menadzer.username=" + username;
     }
 
  
-
+ /**
+     * Vraća string reprezentaciju menadžer sa imenom i prezimenom.
+     * 
+     * @return String reprezentacija menadžera.
+     
+     */
     @Override
     public String toString() {
         return "Menadzer{" + "id=" + id + ", username=" + username + ", password=" + password + ", ime=" + ime + ", prezime=" + prezime + '}';
